9933da995a385b050b39505a53f8dda974f83e54
abdul <abdul@pop-os.localdomain>
2019-06-11 15:07:48 +0530

    .

---
+++ main.js
@@ -0,0 +1,12 @@
+$('document').ready(function () {
+
+    var typed = new Typed('#typed', {
+        strings: ['Abdul Rashid.', 'a Gamer.', 'a Web Developer.', 'a Data Scientist.', 'Batman.'],
+        backSpeed: 75,
+        typeSpeed: 100,
+        loop: true,
+    })
+
+})
+
+
